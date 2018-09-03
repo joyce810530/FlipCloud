14d0a297b1378d4d0fe5418627b57943767bdab3
Chia-Yu Chen <joyce810530@yahoo.com.tw>
2018-09-03 11:02:15 +0800

    SA2TP1 Added

--- cloudtw/production_burnfirmware.js
+++ cloudtw/production_burnfirmware.js
@@ -1,6 +1,7 @@
-<script>var $ = jQuery.noConflict();</script>
-<script>
- var extId = "fmpdgdpnholklpaibdlfhicdbbendjkp";
+< script >
+    var $ = jQuery.noConflict(); < /script> <
+script >
+    var extId = "fmpdgdpnholklpaibdlfhicdbbendjkp";
 var port = chrome.runtime.connect(extId);
 var selectedPort = "ttyUSB0";
 var lastPorts = [];
@@ -12,6 +13,7 @@ searchPort();
 $("#portSelect").mouseover(function () {
     searchPort();
 });
+
 function portsAreSame(x, y) {
     var objectsAreSame = true;
     if ((x.length != y.length) || (x.length == 0) || (y.length == 0))
@@ -24,106 +26,158 @@ function portsAreSame(x, y) {
     }
     return objectsAreSame;
 }
-  function burnFirmware(){
+
+function burnFirmware() {
     var portIndex = $("#portSelect")[0].selectedIndex;
-    if(portIndex!=0){
-      $("#labBurnMsg").text("UPLOADING......");
-      console.log(portIndex);
-      selectedPort = lastPorts[portIndex-1].path;
-      console.log(selectedPort);
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.response);
-          console.log("send hex");
-          port.postMessage({"cmd":"compile","port":selectedPort,"data":this.response});
-        }
-      };
-      xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/E300_firmware_a5.hex", true);
-      xhttp.send();
+    if (portIndex != 0) {
+        $("#labBurnMsg").text("UPLOADING......");
+        console.log(portIndex);
+        selectedPort = lastPorts[portIndex - 1].path;
+        console.log(selectedPort);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.response);
+                console.log("send hex");
+                port.postMessage({
+                    "cmd": "compile",
+                    "port": selectedPort,
+                    "data": this.response
+                });
+            }
+        };
+        xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/E300_firmware_a5.hex", true);
+        xhttp.send();
     }
-  }
-   function burnFirmwaret3(){
+}
+
+function burnFirmwaret3() {
     var portIndex = $("#portSelect")[0].selectedIndex;
-    if(portIndex!=0){
-      $("#labBurnMsg").text("UPLOADING......");
-      console.log(portIndex);
-      selectedPort = lastPorts[portIndex-1].path;
-      console.log(selectedPort);
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.response);
-          console.log("send hex");
-          port.postMessage({"cmd":"compile","port":selectedPort,"data":this.response});
-        }
-      };
-      xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/SA1_T3.hex", true);
-      xhttp.send();
+    if (portIndex != 0) {
+        $("#labBurnMsg").text("UPLOADING......");
+        console.log(portIndex);
+        selectedPort = lastPorts[portIndex - 1].path;
+        console.log(selectedPort);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.response);
+                console.log("send hex");
+                port.postMessage({
+                    "cmd": "compile",
+                    "port": selectedPort,
+                    "data": this.response
+                });
+            }
+        };
+        xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/SA1_T3.hex", true);
+        xhttp.send();
     }
-  }
-   function burnFirmwaret4(){
+}
+
+function burnFirmwaret4() {
     var portIndex = $("#portSelect")[0].selectedIndex;
-    if(portIndex!=0){
-      $("#labBurnMsg").text("UPLOADING......");
-      console.log(portIndex);
-      selectedPort = lastPorts[portIndex-1].path;
-      console.log(selectedPort);
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.response);
-          console.log("send hex");
-          port.postMessage({"cmd":"compile","port":selectedPort,"data":this.response});
-        }
-      };
-      xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/SA1_T4.hex", true);
-      xhttp.send();
+    if (portIndex != 0) {
+        $("#labBurnMsg").text("UPLOADING......");
+        console.log(portIndex);
+        selectedPort = lastPorts[portIndex - 1].path;
+        console.log(selectedPort);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.response);
+                console.log("send hex");
+                port.postMessage({
+                    "cmd": "compile",
+                    "port": selectedPort,
+                    "data": this.response
+                });
+            }
+        };
+        xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/SA1_T4.hex", true);
+        xhttp.send();
     }
-  }
-  function burnFirmwaret5(){
+}
+
+function burnFirmwaret5() {
     var portIndex = $("#portSelect")[0].selectedIndex;
-    if(portIndex!=0){
-      $("#labBurnMsg").text("UPLOADING......");
-      console.log(portIndex);
-      selectedPort = lastPorts[portIndex-1].path;
-      console.log(selectedPort);
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.response);
-          console.log("send hex");
-          port.postMessage({"cmd":"compile","port":selectedPort,"data":this.response});
-        }
-      };
-      xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/SA1_T5.hex", true);
-      xhttp.send();
+    if (portIndex != 0) {
+        $("#labBurnMsg").text("UPLOADING......");
+        console.log(portIndex);
+        selectedPort = lastPorts[portIndex - 1].path;
+        console.log(selectedPort);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.response);
+                console.log("send hex");
+                port.postMessage({
+                    "cmd": "compile",
+                    "port": selectedPort,
+                    "data": this.response
+                });
+            }
+        };
+        xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/SA1_T5.hex", true);
+        xhttp.send();
     }
-  }
-   function burnFirmwaret6(){
+}
+
+function burnFirmwaret6() {
     var portIndex = $("#portSelect")[0].selectedIndex;
-    if(portIndex!=0){
-      $("#labBurnMsg").text("UPLOADING......");
-      console.log(portIndex);
-      selectedPort = lastPorts[portIndex-1].path;
-      console.log(selectedPort);
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.response);
-          console.log("send hex");
-          port.postMessage({"cmd":"compile","port":selectedPort,"data":this.response});
-        }
-      };
-      xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/SA1_T6.hex", true);
-      xhttp.send();
+    if (portIndex != 0) {
+        $("#labBurnMsg").text("UPLOADING......");
+        console.log(portIndex);
+        selectedPort = lastPorts[portIndex - 1].path;
+        console.log(selectedPort);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.response);
+                console.log("send hex");
+                port.postMessage({
+                    "cmd": "compile",
+                    "port": selectedPort,
+                    "data": this.response
+                });
+            }
+        };
+        xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/SA1_T6.hex", true);
+        xhttp.send();
     }
-  }
+}
+
+function burnFirmwaresa2t1() {
+    var portIndex = $("#portSelect")[0].selectedIndex;
+    if (portIndex != 0) {
+        $("#labBurnMsg").text("UPLOADING......");
+        console.log(portIndex);
+        selectedPort = lastPorts[portIndex - 1].path;
+        console.log(selectedPort);
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.response);
+                console.log("send hex");
+                port.postMessage({
+                    "cmd": "compile",
+                    "port": selectedPort,
+                    "data": this.response
+                });
+            }
+        };
+        xhttp.open("GET", "https://cloudtw.fliprobot.com/firmware/L3SA2TP1_20180824.hex", true);
+        xhttp.send();
+    }
+}
 
 function searchPort() {
     console.log("searchPort");
     try {
-        port.postMessage({ "cmd": "searchPort", "data": "" });
+        port.postMessage({
+            "cmd": "searchPort",
+            "data": ""
+        });
     } catch (error) {
         console.log(error);
         if (checkCrmOs) {
@@ -133,8 +187,7 @@ function searchPort() {
             setPortListener();
             checkCrmOs = false;
             searchPort();
-        }
-        else {
+        } else {
             console.log("Please install extension");
         }
     }
@@ -145,8 +198,7 @@ function setPortListener() {
         console.log("web page received: ", msg.cmd);
         if (msg.cmd == "compile") {
             console.log("web page received: ", msg.data);
-        }
-        else if (msg.cmd == "searchPort") {
+        } else if (msg.cmd == "searchPort") {
             var ports = msg.data;
             ports.sort(function (a, b) {
                 a = a.path.replace("COM", "");
@@ -167,8 +219,7 @@ function setPortListener() {
                 }
                 lastPorts = ports;
             }
-        }
-        else if (msg.cmd == "upload") {
+        } else if (msg.cmd == "upload") {
             switch (msg.upload_status) {
                 case "UPLOAD_SUCCESS":
                     $("#labBurnMsg").text("您已成功完成燒錄。");
@@ -183,4 +234,5 @@ function setPortListener() {
     });
 }
 
-</script>
+<
+/script>
