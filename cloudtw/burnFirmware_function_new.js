2a93d29659c9a8267f489eee06cbf08a2fe7d626
Chia-Yu Chen <joyce810530@yahoo.com.tw>
2019-01-21 15:23:08 +0800

    FlipCloud TW burn firmware.js

---
+++ cloudtw/burnFirmware_function_new.js
@@ -0,0 +1,9 @@
+<div class="bburn"><select id="boardSelect" class="add_select add_referral ">
+<option disabled="disabled" selected="selected" value="">選擇板子</option>
+<option value="E300">E300</option>
+</select><br />
+<select id="portSelect" class="add_select add_referral">
+<option disabled="disabled" selected="selected" value="">選擇端口</option>
+</select><br />
+<button id="burnBtn" class="btn btn-primary" type="button" onclick="burnFirmware('L3SA2TP1_20180824')">燒錄</button><br />
+<label id="labBurnMsg"></label></div>
